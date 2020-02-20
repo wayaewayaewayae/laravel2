14ee533924629f493ff2685de04ce932756448ff
reiynaldyy <reiynaldy321@gmail.com>
2020-02-20 12:47:36 +0700

    laravel2

---
+++ database/migrations/2020_02_20_041526_create_hobis_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHobisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hobis', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('hobi');
+
+        });
+            Schema::create('mahasiswa_hobi', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreign('id_mahasiswa')->references('id')
+                  ->on('mahasiswas')->onDelete('cascade');
+            $table->foreign('id_hobi')->references('id')
+                  ->on('hobis')->onDelete('cascade');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hobis');
+        Schema::dropIfExists('mahasiswa_hobi');
+    }
+}
